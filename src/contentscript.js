dc8785ee878a0e9a035248b1903d6ecacf826b62
Maripo GODA <avenafatua@gmail.com>
2012-07-03 19:40:21 +0900

    Add test button

--- src/contentscript.js
+++ src/contentscript.js
@@ -1,3 +1,23 @@
 // mew mew mew
 var audio = new Audio(chrome.extension.getURL("audio/nya.mp3"));
-audio.play();
+
+function playSound ()
+{
+	audio.play();
+}
+function uiTest ()
+{
+	var button = document.createElement('INPUT');
+	button.type = "BUTTON";
+	with (button.style)
+	{
+		position = "absolute";
+		left = "0px";
+		top = "0px";
+		zIndex = 9999;
+	}
+	button.value = "Mew";
+	button.addEventListener ("click", playSound, false);
+	document.body.appendChild(button);
+}
+uiTest();
