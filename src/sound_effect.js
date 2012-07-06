d85c3759c663a74f8658920067e7738393f7dee4
Maripo GODA <avenafatua@gmail.com>
2012-07-06 12:16:26 +0900

    Add class skeleton

---
+++ src/sound_effect.js
@@ -0,0 +1,30 @@
+var SoundEffect = function ()
+{
+};
+
+
+SoundEffect.prototype.play = function ()
+{
+
+};
+
+
+SoundEffect.prototype.save = function ()
+{
+
+};
+
+SoundEffect.prototype.delete = function ()
+{
+
+};
+
+// JSON save & load
+SoundEffect.saveAll = function ()
+{
+
+};
+SoundEffect.loadAll = function ()
+{
+
+};
