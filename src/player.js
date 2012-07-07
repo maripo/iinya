95a166e5ee1e40f5ca9a79959e9d4e96783b1ec7
Maripo GODA <avenafatua@gmail.com>
2012-07-07 12:19:42 +0900

    ガブ嬢のプロフィール + 声を追加

--- src/player.js
+++ src/player.js
@@ -3,7 +3,10 @@ var audios = [
 	new Audio(chrome.extension.getURL("audio/dora.mp3")),
 	new Audio(chrome.extension.getURL("audio/dora2.mp3")),
 	new Audio(chrome.extension.getURL("audio/gustav.mp3")),
-	new Audio(chrome.extension.getURL("audio/gustav2.mp3"))
+	new Audio(chrome.extension.getURL("audio/gustav2.mp3")),
+	new Audio(chrome.extension.getURL("audio/gabu0.mp3")),
+	new Audio(chrome.extension.getURL("audio/gabu1.mp3")),
+	new Audio(chrome.extension.getURL("audio/gabu2.mp3"))
 	];
 
 function playSound ()
