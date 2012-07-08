f24f0f52297e625d972ed7aaff83810938f60253
Maripo GODA <avenafatua@gmail.com>
2012-07-09 00:23:34 +0900

    Add Label getter / setter

--- src/player.js
+++ src/player.js
@@ -17,3 +17,23 @@ Player.setMasterVolume = function (value)
 {
 	localStorage.masterVolume = value;
 }
+
+var Label = {};
+var LABEL_FROM_DEFAULT = "いいね";
+var LABEL_TO_DEFAULT = "いいニャ";
+Label.getLabelFrom = function ()
+{
+	return localStorage.labelFrom | LABEL_FROM_DEFAULT;
+};
+Label.setLabelFrom = function (value)
+{
+	localStorage.labelFrom = value;
+};
+Label.getLabelTo = function ()
+{
+	return localStorage.labelTo | LABEL_TO_DEFAULT;
+};
+Label.setLabelTo = function (value)
+{
+	localStorage.labelTo = value;
+};
